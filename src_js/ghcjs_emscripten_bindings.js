377b766f002f3188f4f7ae12d1ef7024d8ee689c
Marcus Ofenhed <marcus@conditionraise.se>
2017-04-18 06:50:19 +0200

    Improved performance on all hashing algorithms by defining additional pontier types. The current pointer types are:
    * ptr: A regular pointer.
    * c_ptr: The data in this pointer will not be changed by the called
    function, thus no copy back is performed. This is only allowed to be
    used on const pointers.
    * dc_ptr: Don't care pointer. For now an alias for c_ptr to be used when
    the data is not assumed to be changed. This is separated for
    verification purposes.
    * o_ptr: Only the output value of this pointer matters, so input value
    will not be copied.

--- src_js/ghcjs_emscripten_bindings.js
+++ src_js/ghcjs_emscripten_bindings.js
@@ -1,6 +1,6 @@
 function emscripten_ptr_to_haskell_ptr(ptr, /* optional */ length) {
   var str = Pointer_stringify(ptr, length);
-  var iArr = intArrayFromString(str, true);
+  var iArr = intArrayFromString(str, false);
   _free(str);
   var array = h$newByteArray(iArr.length);
   array.u8.set(iArr);
@@ -11,22 +11,28 @@ function write_emscripten_ptr_to_haskell_ptr(e_ptr, h_ptr, length) {
   h_ptr.u8.set(Module.HEAPU8.subarray(e_ptr, e_ptr + length));
 }
 
-function c_ptr(ptr) {
-  var ret = Runtime.stackAlloc(ptr.len);
-  Module.HEAPU8.set(ptr.u8, ret);
-  return [ret];
-}
-
-function ptr(ptr) {
+function ptr_(ptr, copy_before, copy_after) {
   if (ptr == null) {
     return [null];
   }
   var ret = Runtime.stackAlloc(ptr.len);
-  Module.HEAPU8.set(ptr.u8, ret);
-  return [ret, function() {
+  if (copy_before) {
+    Module.HEAPU8.set(ptr.u8, ret);
+  }
+  return copy_after ? [ret, function() {
     write_emscripten_ptr_to_haskell_ptr(ret, ptr, ptr.len);
-  }];
+  }] : [ret];
 }
+
+var c_ptr = function(x) { return ptr_(x, true, false); }
+var ptr = function(x) { return ptr_(x, true, true); }
+var o_ptr = function(x) { return ptr_(x, false, true); }
+var dc_ptr = c_ptr;
+
+function fwd(x) {
+  return [x];
+}
+
 
 function zipMap(a, b) {
   var arr = [];
@@ -40,10 +46,6 @@ function zipMap(a, b) {
       arr.push(res[0]);
     }
   return [arr, function() { for (var f in finalizer.reverse()) finalizer[f]() }];
-}
-
-function fwd(x) {
-  return [x];
 }
 
 function runFunc(func, funcname, operations) {
@@ -75,9 +77,6 @@ function str2ab(str) {
 ////h$inflateInit2_       =runFunc(_inflateInit2_,       "inflateInit2_");
 //h$inflateReset        =runFunc(_inflateReset,        "inflateReset");
 //h$inflateSetDictionary=runFunc(_inflateSetDictionary,"inflateSetDictionary");
-////h$zlibVersion         =function() {
-////  return emscripten_ptr_to_haskell_ptr(_zlibVersion());
-////};
 //h$inflateInit2_             =function(v1, v2 ,v3 ,v4, v5, v6) {
 //  o1 = haskell_ptr_to_emscripten_ptr(v1);
 //  o4 = haskell_ptr_to_emscripten_ptr(v4);
@@ -94,12 +93,11 @@ function str2ab(str) {
 //
 //
 
-
-h$cryptonite_sha1_init = runFunc(_cryptonite_sha1_init, "cryptonite_sha1_init", [ptr, null]);
+h$cryptonite_sha1_init = runFunc(_cryptonite_sha1_init, "cryptonite_sha1_init", [o_ptr, null]);
 h$cryptonite_sha1_update = runFunc(_cryptonite_sha1_update, "cryptonite_sha1_update", [ptr, null, c_ptr, null, fwd])
-h$cryptonite_sha1_finalize = runFunc(_cryptonite_sha1_finalize, "cryptonite_sha1_finalize", [c_ptr, null, ptr, null])
-h$cryptonite_aes_decrypt_cbc = runFunc(_cryptonite_aes_decrypt_cbc, "cryptonite_aes_decrypt_cbc", [ptr, null, ptr, null, ptr, null, c_ptr, null, fwd])
-h$cryptonite_aes_encrypt_cbc = runFunc(_cryptonite_aes_encrypt_cbc, "cryptonite_aes_encrypt_cbc", [ptr, null, ptr, null, ptr, null, c_ptr, null, fwd])
+h$cryptonite_sha1_finalize = runFunc(_cryptonite_sha1_finalize, "cryptonite_sha1_finalize", [dc_ptr, null, o_ptr, null])
+h$cryptonite_aes_decrypt_cbc = runFunc(_cryptonite_aes_decrypt_cbc, "cryptonite_aes_decrypt_cbc", [ptr, null, ptr, null, ptr, null, dc_ptr, null, fwd])
+h$cryptonite_aes_encrypt_cbc = runFunc(_cryptonite_aes_encrypt_cbc, "cryptonite_aes_encrypt_cbc", [ptr, null, ptr, null, ptr, null, dc_ptr, null, fwd])
 h$cryptonite_aes_decrypt_ecb = runFunc(_cryptonite_aes_decrypt_ecb, "cryptonite_aes_decrypt_ecb", [ptr, null, ptr, null, c_ptr, null, fwd])
 h$cryptonite_aes_encrypt_ecb = runFunc(_cryptonite_aes_encrypt_ecb, "cryptonite_aes_encrypt_ecb", [ptr, null, ptr, null, c_ptr, null, fwd])
 h$cryptonite_aes_encrypt_ctr = runFunc(_cryptonite_aes_encrypt_ctr, "cryptonite_aes_encrypt_ctr", [ptr, null, ptr, null, ptr, null, ptr, null, fwd])
@@ -114,19 +112,19 @@ h$cryptonite_aes_ocb_decrypt = runFunc(_cryptonite_aes_ocb_decrypt, "cryptonite_
 h$cryptonite_aes_ocb_encrypt = runFunc(_cryptonite_aes_ocb_encrypt, "cryptonite_aes_ocb_encrypt", [ptr, null, ptr, null, ptr, null, ptr, null, fwd])
 h$cryptonite_aes_ocb_finish = runFunc(_cryptonite_aes_ocb_finish, "cryptonite_aes_ocb_finish", [ptr, null, ptr, null, ptr, null])
 h$cryptonite_aes_ocb_init = runFunc(_cryptonite_aes_ocb_init, "cryptonite_aes_ocb_init", [ptr, null, ptr, null, ptr, null, fwd])
-h$cryptonite_blake2b_finalize = runFunc(_cryptonite_blake2b_finalize, "cryptonite_blake2b_finalize", [ptr, null, fwd, ptr, null])
-h$cryptonite_blake2b_init = runFunc(_cryptonite_blake2b_init, "cryptonite_blake2b_init", [ptr, null, fwd])
-h$cryptonite_blake2b_update = runFunc(_cryptonite_blake2b_update, "cryptonite_blake2b_update", [ptr, null, ptr, null, fwd])
-h$cryptonite_curve25519_donna = runFunc(_cryptonite_curve25519_donna, "cryptonite_curve25519_donna", [ptr, null, ptr, null, ptr, null])
-h$cryptonite_sha3_finalize = runFunc(_cryptonite_sha3_finalize, "cryptonite_sha3_finalize", [ptr, null, fwd, ptr, null])
-h$cryptonite_sha3_init = runFunc(_cryptonite_sha3_init, "cryptonite_sha3_init", [ptr, null, fwd])
-h$cryptonite_sha3_update = runFunc(_cryptonite_sha3_update, "cryptonite_sha3_update", [ptr, null, ptr, null, fwd])
-h$cryptonite_sha512_finalize = runFunc(_cryptonite_sha512_finalize, "cryptonite_sha512_finalize", [ptr, null, ptr, null])
-h$cryptonite_sha512_init = runFunc(_cryptonite_sha512_init, "cryptonite_sha512_init", [ptr, null])
-h$cryptonite_sha512_update = runFunc(_cryptonite_sha512_update, "cryptonite_sha512_update", [ptr, null, ptr, null, fwd])
-h$cryptonite_skein512_finalize = runFunc(_cryptonite_skein512_finalize, "cryptonite_skein512_finalize", [ptr, null, fwd, ptr, null])
-h$cryptonite_skein512_init = runFunc(_cryptonite_skein512_init, "cryptonite_skein512_init", [ptr, null, fwd])
-h$cryptonite_skein512_update = runFunc(_cryptonite_skein512_update, "cryptonite_skein512_update", [ptr, null, ptr, null, fwd])
-h$cryptonite_ed25519_publickey = runFunc(_cryptonite_ed25519_publickey, "cryptonite_ed25519_publickey", [ptr, null, ptr, null])
-h$cryptonite_ed25519_sign = runFunc(_cryptonite_ed25519_sign, "cryptonite_ed25519_sign", [ptr, null, fwd, ptr, null, ptr, null, ptr, null])
-h$cryptonite_ed25519_sign_open = runFunc(_cryptonite_ed25519_sign_open, "cryptonite_ed25519_sign_open", [ptr, null, fwd, ptr, null, ptr, null])
+h$cryptonite_blake2b_finalize = runFunc(_cryptonite_blake2b_finalize, "cryptonite_blake2b_finalize", [dc_ptr, null, fwd, o_ptr, null])
+h$cryptonite_blake2b_init = runFunc(_cryptonite_blake2b_init, "cryptonite_blake2b_init", [o_ptr, null, fwd])
+h$cryptonite_blake2b_update = runFunc(_cryptonite_blake2b_update, "cryptonite_blake2b_update", [ptr, null, c_ptr, null, fwd])
+h$cryptonite_curve25519_donna = runFunc(_cryptonite_curve25519_donna, "cryptonite_curve25519_donna", [ptr, null, c_ptr, null, c_ptr, null])
+h$cryptonite_sha3_finalize = runFunc(_cryptonite_sha3_finalize, "cryptonite_sha3_finalize", [dc_ptr, null, fwd, o_ptr, null])
+h$cryptonite_sha3_init = runFunc(_cryptonite_sha3_init, "cryptonite_sha3_init", [o_ptr, null, fwd])
+h$cryptonite_sha3_update = runFunc(_cryptonite_sha3_update, "cryptonite_sha3_update", [ptr, null, c_ptr, null, fwd])
+h$cryptonite_sha512_finalize = runFunc(_cryptonite_sha512_finalize, "cryptonite_sha512_finalize", [dc_ptr, null, o_ptr, null])
+h$cryptonite_sha512_init = runFunc(_cryptonite_sha512_init, "cryptonite_sha512_init", [o_ptr, null])
+h$cryptonite_sha512_update = runFunc(_cryptonite_sha512_update, "cryptonite_sha512_update", [ptr, null, c_ptr, null, fwd])
+h$cryptonite_skein512_finalize = runFunc(_cryptonite_skein512_finalize, "cryptonite_skein512_finalize", [dc_ptr, null, fwd, o_ptr, null])
+h$cryptonite_skein512_init = runFunc(_cryptonite_skein512_init, "cryptonite_skein512_init", [o_ptr, null, fwd])
+h$cryptonite_skein512_update = runFunc(_cryptonite_skein512_update, "cryptonite_skein512_update", [ptr, null, c_ptr, null, fwd])
+h$cryptonite_ed25519_publickey = runFunc(_cryptonite_ed25519_publickey, "cryptonite_ed25519_publickey", [c_ptr, null, o_ptr, null])
+h$cryptonite_ed25519_sign = runFunc(_cryptonite_ed25519_sign, "cryptonite_ed25519_sign", [c_ptr, null, fwd, c_ptr, null, c_ptr, null, o_ptr, null])
+h$cryptonite_ed25519_sign_open = runFunc(_cryptonite_ed25519_sign_open, "cryptonite_ed25519_sign_open", [c_ptr, null, fwd, c_ptr, null, c_ptr, null])
