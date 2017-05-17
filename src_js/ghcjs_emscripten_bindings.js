181b0a7bddfd6eac09ecc61d0e4bbd5529b6ef68
Marcus Ofenhed <marcus@conditionraise.se>
2017-05-17 14:12:24 +0200

    Whitespace changes only. Fixed some indentation.

--- src_js/ghcjs_emscripten_bindings.js
+++ src_js/ghcjs_emscripten_bindings.js
@@ -1,100 +1,100 @@
 function init_ghcjs_emscripten_bindings() {
-function emscripten_ptr_to_haskell_ptr(ptr, /* optional */ length) {
-  return h$wrapBuffer(Module.buffer, true, ptr, length);
-}
+  function emscripten_ptr_to_haskell_ptr(ptr, /* optional */ length) {
+    return h$wrapBuffer(Module.buffer, true, ptr, length);
+  }
 
-function write_emscripten_ptr_to_haskell_ptr(e_ptr, h_ptr) {
-  h_ptr.u8.set(Module.HEAPU8.subarray(e_ptr, e_ptr + h_ptr.len));
-}
+  function write_emscripten_ptr_to_haskell_ptr(e_ptr, h_ptr) {
+    h_ptr.u8.set(Module.HEAPU8.subarray(e_ptr, e_ptr + h_ptr.len));
+  }
 
-function ptr_(ptr, copy_before, copy_after) {
-  if (ptr == null) {
-    return [null];
-  }
-  var ret = Runtime.stackAlloc(ptr.len*2);
-  if (copy_before) {
-    Module.HEAPU8.set(ptr.u8, ret);
-  }
-  return copy_after ? [ret, function() {
-    write_emscripten_ptr_to_haskell_ptr(ret, ptr);
-  }] : [ret];
-}
-
-function heapptr_(ptr, copy_before, copy_after) {
-  if (ptr == null) {
-    return [null];
-  }
-  var ret = Module.allocate(ptr.u8, 'i8', ALLOC_NORMAL);
-  if (ret == 0) {
-    throw "Failed to allocate " + ptr.len + " bytes";
-  }
-  return [ret, function() {
-    if (copy_after) {
+  function ptr_(ptr, copy_before, copy_after) {
+    if (ptr == null) {
+      return [null];
+    }
+    var ret = Runtime.stackAlloc(ptr.len*2);
+    if (copy_before) {
+      Module.HEAPU8.set(ptr.u8, ret);
+    }
+    return copy_after ? [ret, function() {
       write_emscripten_ptr_to_haskell_ptr(ret, ptr);
-    }
-    Module._free(ret);
-  }];
-}
-
-var c_ptr = function(x) { return heapptr_(x, true, false); }
-var ptr = function(x) { return heapptr_(x, true, true); }
-var o_ptr = function(x) { return heapptr_(x, false, true); }
-c_ptr = ptr;
-o_ptr = ptr;
-var dc_ptr = c_ptr;
-
-function fwd(x) {
-  return [x];
-}
-
-
-function zipMap(a, b) {
-  var arr = [];
-  var finalizer = [];
-  for (var key in a)
-    if (b[key] != null) {
-      var res = b[key](a[key]);
-      if (res.length == 2) {
-        finalizer.push(res[1]);
-      }
-      arr.push(res[0]);
-    }
-  return [arr, function() { for (var f in finalizer) finalizer[f]() }];
-}
-
-function runFunc(func, funcname, operations) {
-  return function() {
-    if (arguments.length != operations.length) {
-      alert("Arguments missmatch for function " + funcname);
-    }
-    var stackTop = Runtime.stackSave();
-    var zipped = zipMap(arguments, operations);
-    var ret = func.apply(this, zipped[0]);
-    zipped[1]();
-    Runtime.stackRestore(stackTop);
-    return ret;
+    }] : [ret];
   }
-}
 
-h$cryptonite_aes_encrypt_ctr = runFunc(_cryptonite_aes_encrypt_ctr, "cryptonite_aes_encrypt_ctr", [o_ptr, null, c_ptr, null, c_ptr, null, c_ptr, null, fwd])
-h$cryptonite_aes_initkey = runFunc(_cryptonite_aes_initkey, "cryptonite_aes_initkey", [ptr, null, ptr, null, fwd])
-h$cryptonite_blake2b_finalize = runFunc(_cryptonite_blake2b_finalize, "cryptonite_blake2b_finalize", [dc_ptr, null, fwd, o_ptr, null])
-h$cryptonite_blake2b_init = runFunc(_cryptonite_blake2b_init, "cryptonite_blake2b_init", [o_ptr, null, fwd])
-h$cryptonite_blake2b_update = runFunc(_cryptonite_blake2b_update, "cryptonite_blake2b_update", [ptr, null, c_ptr, null, fwd])
-h$cryptonite_curve25519_donna = runFunc(_cryptonite_curve25519_donna, "cryptonite_curve25519_donna", [ptr, null, c_ptr, null, c_ptr, null])
-h$cryptonite_ed25519_publickey = runFunc(_cryptonite_ed25519_publickey, "cryptonite_ed25519_publickey", [c_ptr, null, o_ptr, null])
-h$cryptonite_ed25519_sign_open = runFunc(_cryptonite_ed25519_sign_open, "cryptonite_ed25519_sign_open", [c_ptr, null, fwd, c_ptr, null, c_ptr, null])
-h$cryptonite_ed25519_sign = runFunc(_cryptonite_ed25519_sign, "cryptonite_ed25519_sign", [c_ptr, null, fwd, c_ptr, null, c_ptr, null, o_ptr, null])
-h$cryptonite_sha1_finalize = runFunc(_cryptonite_sha1_finalize, "cryptonite_sha1_finalize", [dc_ptr, null, o_ptr, null])
-h$cryptonite_sha1_init = runFunc(_cryptonite_sha1_init, "cryptonite_sha1_init", [o_ptr, null]);
-h$cryptonite_sha1_update = runFunc(_cryptonite_sha1_update, "cryptonite_sha1_update", [ptr, null, c_ptr, null, fwd])
-h$cryptonite_sha3_finalize = runFunc(_cryptonite_sha3_finalize, "cryptonite_sha3_finalize", [dc_ptr, null, fwd, o_ptr, null])
-h$cryptonite_sha3_init = runFunc(_cryptonite_sha3_init, "cryptonite_sha3_init", [o_ptr, null, fwd])
-h$cryptonite_sha3_update = runFunc(_cryptonite_sha3_update, "cryptonite_sha3_update", [ptr, null, c_ptr, null, fwd])
-h$cryptonite_sha512_finalize = runFunc(_cryptonite_sha512_finalize, "cryptonite_sha512_finalize", [dc_ptr, null, o_ptr, null])
-h$cryptonite_sha512_init = runFunc(_cryptonite_sha512_init, "cryptonite_sha512_init", [o_ptr, null])
-h$cryptonite_sha512_update = runFunc(_cryptonite_sha512_update, "cryptonite_sha512_update", [ptr, null, c_ptr, null, fwd])
-h$cryptonite_skein512_finalize = runFunc(_cryptonite_skein512_finalize, "cryptonite_skein512_finalize", [dc_ptr, null, fwd, o_ptr, null])
-h$cryptonite_skein512_init = runFunc(_cryptonite_skein512_init, "cryptonite_skein512_init", [o_ptr, null, fwd])
-h$cryptonite_skein512_update = runFunc(_cryptonite_skein512_update, "cryptonite_skein512_update", [ptr, null, c_ptr, null, fwd])
+  function heapptr_(ptr, copy_before, copy_after) {
+    if (ptr == null) {
+      return [null];
+    }
+    var ret = Module.allocate(ptr.u8, 'i8', ALLOC_NORMAL);
+    if (ret == 0) {
+      throw "Failed to allocate " + ptr.len + " bytes";
+    }
+    return [ret, function() {
+      if (copy_after) {
+        write_emscripten_ptr_to_haskell_ptr(ret, ptr);
+      }
+      Module._free(ret);
+    }];
+  }
+
+  var c_ptr = function(x) { return heapptr_(x, true, false); }
+  var ptr = function(x) { return heapptr_(x, true, true); }
+  var o_ptr = function(x) { return heapptr_(x, false, true); }
+  c_ptr = ptr;
+  o_ptr = ptr;
+  var dc_ptr = c_ptr;
+
+  function fwd(x) {
+    return [x];
+  }
+
+
+  function zipMap(a, b) {
+    var arr = [];
+    var finalizer = [];
+    for (var key in a)
+      if (b[key] != null) {
+        var res = b[key](a[key]);
+        if (res.length == 2) {
+          finalizer.push(res[1]);
+        }
+        arr.push(res[0]);
+      }
+    return [arr, function() { for (var f in finalizer) finalizer[f]() }];
+  }
+
+  function runFunc(func, funcname, operations) {
+    return function() {
+      if (arguments.length != operations.length) {
+        alert("Arguments missmatch for function " + funcname);
+      }
+      var stackTop = Runtime.stackSave();
+      var zipped = zipMap(arguments, operations);
+      var ret = func.apply(this, zipped[0]);
+      zipped[1]();
+      Runtime.stackRestore(stackTop);
+      return ret;
+    }
+  }
+
+  h$cryptonite_aes_encrypt_ctr = runFunc(_cryptonite_aes_encrypt_ctr, "cryptonite_aes_encrypt_ctr", [o_ptr, null, c_ptr, null, c_ptr, null, c_ptr, null, fwd])
+  h$cryptonite_aes_initkey = runFunc(_cryptonite_aes_initkey, "cryptonite_aes_initkey", [ptr, null, ptr, null, fwd])
+  h$cryptonite_blake2b_finalize = runFunc(_cryptonite_blake2b_finalize, "cryptonite_blake2b_finalize", [dc_ptr, null, fwd, o_ptr, null])
+  h$cryptonite_blake2b_init = runFunc(_cryptonite_blake2b_init, "cryptonite_blake2b_init", [o_ptr, null, fwd])
+  h$cryptonite_blake2b_update = runFunc(_cryptonite_blake2b_update, "cryptonite_blake2b_update", [ptr, null, c_ptr, null, fwd])
+  h$cryptonite_curve25519_donna = runFunc(_cryptonite_curve25519_donna, "cryptonite_curve25519_donna", [ptr, null, c_ptr, null, c_ptr, null])
+  h$cryptonite_ed25519_publickey = runFunc(_cryptonite_ed25519_publickey, "cryptonite_ed25519_publickey", [c_ptr, null, o_ptr, null])
+  h$cryptonite_ed25519_sign_open = runFunc(_cryptonite_ed25519_sign_open, "cryptonite_ed25519_sign_open", [c_ptr, null, fwd, c_ptr, null, c_ptr, null])
+  h$cryptonite_ed25519_sign = runFunc(_cryptonite_ed25519_sign, "cryptonite_ed25519_sign", [c_ptr, null, fwd, c_ptr, null, c_ptr, null, o_ptr, null])
+  h$cryptonite_sha1_finalize = runFunc(_cryptonite_sha1_finalize, "cryptonite_sha1_finalize", [dc_ptr, null, o_ptr, null])
+  h$cryptonite_sha1_init = runFunc(_cryptonite_sha1_init, "cryptonite_sha1_init", [o_ptr, null]);
+  h$cryptonite_sha1_update = runFunc(_cryptonite_sha1_update, "cryptonite_sha1_update", [ptr, null, c_ptr, null, fwd])
+  h$cryptonite_sha3_finalize = runFunc(_cryptonite_sha3_finalize, "cryptonite_sha3_finalize", [dc_ptr, null, fwd, o_ptr, null])
+  h$cryptonite_sha3_init = runFunc(_cryptonite_sha3_init, "cryptonite_sha3_init", [o_ptr, null, fwd])
+  h$cryptonite_sha3_update = runFunc(_cryptonite_sha3_update, "cryptonite_sha3_update", [ptr, null, c_ptr, null, fwd])
+  h$cryptonite_sha512_finalize = runFunc(_cryptonite_sha512_finalize, "cryptonite_sha512_finalize", [dc_ptr, null, o_ptr, null])
+  h$cryptonite_sha512_init = runFunc(_cryptonite_sha512_init, "cryptonite_sha512_init", [o_ptr, null])
+  h$cryptonite_sha512_update = runFunc(_cryptonite_sha512_update, "cryptonite_sha512_update", [ptr, null, c_ptr, null, fwd])
+  h$cryptonite_skein512_finalize = runFunc(_cryptonite_skein512_finalize, "cryptonite_skein512_finalize", [dc_ptr, null, fwd, o_ptr, null])
+  h$cryptonite_skein512_init = runFunc(_cryptonite_skein512_init, "cryptonite_skein512_init", [o_ptr, null, fwd])
+  h$cryptonite_skein512_update = runFunc(_cryptonite_skein512_update, "cryptonite_skein512_update", [ptr, null, c_ptr, null, fwd])
 };
--- src_js/random.js
+++ src_js/random.js
@@ -1,104 +1,104 @@
 (function() {
-function stringToUint(string) {
-  if (typeof this.encoder !== "undefined") {
-    return this.encoder.encode(string);
+  function stringToUint(string) {
+    if (typeof this.encoder !== "undefined") {
+      return this.encoder.encode(string);
+    }
+    if (typeof TextEncoder !== "undefined") {
+      this.encoder = new TextEncoder("ascii");
+      return stringToUint(string);
+    }
+    var charList = string.split(''),
+      uintArray = [];
+    for (var i = 0; i < charList.length; i++) {
+      uintArray.push(charList[i].charCodeAt(0));
+    }
+    return new Uint8Array(uintArray);
   }
-  if (typeof TextEncoder !== "undefined") {
-    this.encoder = new TextEncoder("ascii");
-    return stringToUint(string);
-  }
-  var charList = string.split(''),
-    uintArray = [];
-  for (var i = 0; i < charList.length; i++) {
-    uintArray.push(charList[i].charCodeAt(0));
-  }
-  return new Uint8Array(uintArray);
-}
 
-function uintToString(uintArray) {
-  if (typeof this.decoder !== "undefined") {
-    return this.decoder.decode(uintArray);
+  function uintToString(uintArray) {
+    if (typeof this.decoder !== "undefined") {
+      return this.decoder.decode(uintArray);
+    }
+    if (typeof TextDecoder !== "undefined") {
+      this.decoder = new TextDecoder("ascii");
+      return uintToString(uintArray);
+    }
+    var decodedString = String.fromCharCode.apply(null, uintArray);
+    return decodedString;
   }
-  if (typeof TextDecoder !== "undefined") {
-    this.decoder = new TextDecoder("ascii");
-    return uintToString(uintArray);
+  MyRandom = {
+    sha512: function(data) {
+      var stackTop = Runtime.stackSave();
+      var state = Runtime.stackAlloc(256);
+      var maxlength = 0;
+      for (var key in data) {
+        if (data[key].length > maxlength) {
+          maxlength = data[key].length + 1;
+        }
+      }
+      var stackData = Runtime.stackAlloc(maxlength);
+      var returnedData = Runtime.stackAlloc(64);
+      _cryptonite_sha512_init(state);
+      for (var key in data) {
+        var current = data[key];
+        if (typeof current === "string") {
+          current = stringToUint(current);
+        }
+        Module.HEAPU8.set(current, stackData);
+        _cryptonite_sha512_update(state, stackData, data[key].length);
+      }
+      _cryptonite_sha512_finalize(state, returnedData);
+      var ret = Module.HEAPU8.subarray(returnedData, returnedData + 64);
+      Runtime.stackRestore(stackTop);
+      return ret;
+    },
+    getBrowserRandom: function(target) {
+      if (window.crypto && window.crypto.getRandomValues) {
+        window.crypto.getRandomValues(target);
+      } else if (typeof this.notifiedAboutMissingRandom === "undefined") {
+        this.notifiedAboutMissingRandom = true;
+        alert("This browser doesn't provide any cryptographically secure random.");
+      }
+    },
+    state: new Uint8Array(64),
+    getSingleRandom: function() {
+      var newRandom = new Uint8Array(64);
+      this.getBrowserRandom(newRandom);
+      var result = this.sha512(["outp", this.state, newRandom]);
+      this.state.set(this.sha512(["stat", this.state, newRandom]));
+      return result;
+    },
+    getRandom: function(target) {
+      if (typeof target === "number") {
+        var result = new Uint8Array(target);
+      } else {
+        var result = target;
+      }
+      for (var i = 0; i < result.length; i += 64) {
+        result.set(this.getSingleRandom().subarray(0, result.length - i), i);
+      }
+      if (typeof target === "number") {
+        return uintToString(result);
+      }
+    },
+    addSalt: function(salt) {
+      var newRandom = new Uint8Array(64);
+      this.getBrowserRandom(newRandom);
+      var time = window.performance.now ? "" + window.performance.now() : "";
+      var newState = [salt, this.state, newRandom, time];
+      this.state.set(this.sha512(newState));
+    }
   }
-  var decodedString = String.fromCharCode.apply(null, uintArray);
-  return decodedString;
-}
-MyRandom = {
-  sha512: function(data) {
-    var stackTop = Runtime.stackSave();
-    var state = Runtime.stackAlloc(256);
-    var maxlength = 0;
-    for (var key in data) {
-      if (data[key].length > maxlength) {
-        maxlength = data[key].length + 1;
-      }
-    }
-    var stackData = Runtime.stackAlloc(maxlength);
-    var returnedData = Runtime.stackAlloc(64);
-    _cryptonite_sha512_init(state);
-    for (var key in data) {
-      var current = data[key];
-      if (typeof current === "string") {
-        current = stringToUint(current);
-      }
-      Module.HEAPU8.set(current, stackData);
-      _cryptonite_sha512_update(state, stackData, data[key].length);
-    }
-    _cryptonite_sha512_finalize(state, returnedData);
-    var ret = Module.HEAPU8.subarray(returnedData, returnedData + 64);
-    Runtime.stackRestore(stackTop);
-    return ret;
-  },
-  getBrowserRandom: function(target) {
-    if (window.crypto && window.crypto.getRandomValues) {
-      window.crypto.getRandomValues(target);
-    } else if (typeof this.notifiedAboutMissingRandom === "undefined") {
-      this.notifiedAboutMissingRandom = true;
-      alert("This browser doesn't provide any cryptographically secure random.");
-    }
-  },
-  state: new Uint8Array(64),
-  getSingleRandom: function() {
-    var newRandom = new Uint8Array(64);
-    this.getBrowserRandom(newRandom);
-    var result = this.sha512(["outp", this.state, newRandom]);
-    this.state.set(this.sha512(["stat", this.state, newRandom]));
-    return result;
-  },
-  getRandom: function(target) {
-    if (typeof target === "number") {
-      var result = new Uint8Array(target);
+
+  h$cryptonite_cpu_has_rdrand = function() { return 1; }
+  h$cryptonite_get_rand_bytes = function(ptr, ignore, count) {
+    if (count == ptr.length) {
+      MyRandom.getRandom(ptr);
     } else {
-      var result = target;
+      var result = new Uint8Array(count);
+      MyRandom.getRandom(result);
+      ptr.u8.set(result);
     }
-    for (var i = 0; i < result.length; i += 64) {
-      result.set(this.getSingleRandom().subarray(0, result.length - i), i);
-    }
-    if (typeof target === "number") {
-      return uintToString(result);
-    }
-  },
-  addSalt: function(salt) {
-    var newRandom = new Uint8Array(64);
-    this.getBrowserRandom(newRandom);
-    var time = window.performance.now ? "" + window.performance.now() : "";
-    var newState = [salt, this.state, newRandom, time];
-    this.state.set(this.sha512(newState));
+    return count;
   }
-}
-
-h$cryptonite_cpu_has_rdrand = function() { return 1; }
-h$cryptonite_get_rand_bytes = function(ptr, ignore, count) {
-  if (count == ptr.length) {
-    MyRandom.getRandom(ptr);
-  } else {
-    var result = new Uint8Array(count);
-    MyRandom.getRandom(result);
-    ptr.u8.set(result);
-  }
-  return count;
-}
 })();
